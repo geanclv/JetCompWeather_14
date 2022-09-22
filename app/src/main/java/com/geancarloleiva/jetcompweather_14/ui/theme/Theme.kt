4a2a0774affbce0b51eb105f98dc99b636decdb6
geanclv <geanclv@gmail.com>
2022-09-22 17:25:27 -0500

    Adding project dependencies.

--- app/src/main/java/com/geancarloleiva/jetcompweather_14/ui/theme/Theme.kt
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun JetCompWeather_14Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun JetCompWeather_14Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
