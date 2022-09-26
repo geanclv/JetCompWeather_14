0b29112ddab864005b6ef8974376eac525e58231
geanclv <geanclv@gmail.com>
2022-09-26 17:51:18 -0500

    Creating TopAppBar and start using in MainScreen

--- app/src/main/java/com/geancarloleiva/jetcompweather_14/screen/main/WeatherMainScreen.kt
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/screen/main/WeatherMainScreen.kt
@@ -1,10 +1,15 @@
 package com.geancarloleiva.jetcompweather_14.screen.main
 
+import android.util.Log
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.geancarloleiva.jetcompweather_14.data.DataOrException
@@ -37,9 +42,22 @@ fun MainScaffold(
     weather: Weather,
     navController: NavController
 ) {
-    Scaffold(topBar = {
-
-    }) {
+    Scaffold(backgroundColor = Color.White,
+        topBar = {
+            WeatherAppBar(
+                title = "${weather.city.name}, ${weather.city.country}",
+                navController = navController,
+                elevation = 3.dp,
+                //icon = Icons.Default.ArrowBack, //here only for example
+                //action called when in TopAppBar implementation. ID 27676 (just to find and reference)
+                /*onButtonClicked = {
+                    Log.e("GCLV", "MainScaffold: icon Button clicked", )
+                }*/
+            ){
+                //As onButtonClicked is the last param, we can use this lambda
+                Log.e("GCLV", "MainScaffold: icon Button clicked", )
+            }
+        }) {
         //Main content: city selected
         MainContent(data = weather)
     }
---
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/widget/WeatherAppBar.kt
@@ -0,0 +1,89 @@
+package com.geancarloleiva.jetcompweather_14.widget
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.geancarloleiva.jetcompweather_14.R
+
+@Composable
+fun WeatherAppBar(
+    title: String,
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = MaterialTheme.colors.onSecondary,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            )
+        },
+        actions = {
+            if (isMainScreen) {
+                //Search icon
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = stringResource(id = R.string.app_bar_search)
+                    )
+                }
+                //More menu icon
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Rounded.MoreVert,
+                        contentDescription = stringResource(id = R.string.app_bar_menu)
+                    )
+                }
+            }
+        },
+        navigationIcon = {
+            if (icon != null) {
+                Icon(imageVector = icon, contentDescription = title,
+                    tint = MaterialTheme.colors.onSecondary,
+                    modifier = Modifier.clickable {
+                        //When is needed, this should be implemented in the method that called it. ID 27676
+                        onButtonClicked.invoke()
+                    })
+            }
+        },
+        elevation = elevation,
+        backgroundColor = Color.White
+    )
+
+    //Different way to build the AppBar
+    /*TopAppBar(backgroundColor = Color.White) {
+        Text(
+            text = title,
+            color = MaterialTheme.colors.onSecondary,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp
+            )
+        )
+    }*/
+}
