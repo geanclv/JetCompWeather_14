9a7f447559ce300e8f4e98f3d1a074a0abd9f0b1
geanclv <geanclv@gmail.com>
2022-09-26 10:09:19 -0500

    Initiating the app Navigation config: Splash screen

---
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.geancarloleiva.jetcompweather_14.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavoriteScreen,
+    SearchScreen,
+    SettingsScreen
+}
---
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/screen/WeatherSplashScreen.kt
@@ -0,0 +1,10 @@
+package com.geancarloleiva.jetcompweather_14.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherSplashScreen(navController: NavController){
+    Text(text = "Splash")
+}
