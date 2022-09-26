19252b83407bfdbde51918e80f3c67f82be89a3d
geanclv <geanclv@gmail.com>
2022-09-26 12:22:09 -0500

    Starting Retrofit config: method for main call complete Weather

---
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/navigation/WeatherApi.kt
@@ -0,0 +1,18 @@
+package com.geancarloleiva.jetcompweather_14.navigation
+
+import com.geancarloleiva.jetcompweather_14.model.Weather
+import com.geancarloleiva.jetcompweather_14.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface WeatherApi {
+
+    @GET(value = Constants.API_URL)
+    suspend fun getWeather(
+        @Query("q") queryCity: String,
+        @Query("units") units: String = Constants.UNIT_IMPERIAL,
+        @Query("appid") appid: String = Constants.API_KEY
+    ): Weather
+}
---
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.geancarloleiva.jetcompweather_14.utils
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val API_URL = "data/2.5/forecast/daily"
+    const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
+
+    const val UNIT_IMPERIAL = "imperial"
+}
