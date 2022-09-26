5a3e7cf5efa337a105a02327e2c783906e156f27
geanclv <geanclv@gmail.com>
2022-09-26 16:25:35 -0500

    Creating Main ViewModel

---
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/viewmodel/main/MainViewModel.kt
@@ -0,0 +1,41 @@
+package com.geancarloleiva.jetcompweather_14.viewmodel.main
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.geancarloleiva.jetcompweather_14.data.DataOrException
+import com.geancarloleiva.jetcompweather_14.model.Weather
+import com.geancarloleiva.jetcompweather_14.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel
+@Inject constructor(private val repository: WeatherRepository) : ViewModel() {
+
+    val data: MutableState<DataOrException<Weather, Boolean, Exception>> = mutableStateOf(
+        DataOrException(null, true, Exception(""))
+    )
+
+    init {
+        loadWeather()
+    }
+
+    fun loadWeather(){
+        getWeather("Lima")
+    }
+
+    fun getWeather(queryCity: String){
+        viewModelScope.launch {
+            if(queryCity.isEmpty()) return@launch
+
+            data.value.loading = true
+            data.value = repository.getWeather(queryCity)
+
+            if(data.value.data.toString().isNotEmpty())
+                data.value.loading = false
+        }
+    }
+}
