9ee6bd5c42e4216b2c1830d10173f71e71621dcc
Geancarlo <geanclv@gmail.com>
2022-10-12 09:59:20 -0500

    Updating AppModule class to include related methods to Dao and Database.

--- app/src/main/java/com/geancarloleiva/jetcompweather_14/di/AppModule.kt
+++ app/src/main/java/com/geancarloleiva/jetcompweather_14/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.geancarloleiva.jetcompweather_14.di
 
+import android.content.Context
+import androidx.room.Room
+import com.geancarloleiva.jetcompweather_14.data.WeatherDao
+import com.geancarloleiva.jetcompweather_14.data.WeatherDatabase
 import com.geancarloleiva.jetcompweather_14.network.WeatherApi
 import com.geancarloleiva.jetcompweather_14.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -22,5 +27,22 @@ class AppModule {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao {
+        return weatherDatabase.weatherDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): WeatherDatabase {
+        return Room.databaseBuilder(
+            context,
+            WeatherDatabase::class.java,
+            "weather_database"
+        ).fallbackToDestructiveMigration()
+            .build()
     }
 }
